da97b168ba5fc562448b84992a7fcd23eb2aa5e4
jpavioli <justin.avioli@gmail.com>
2019-05-23 12:31:46 -0500

    Done. Object Oriented

--- src/index.js
+++ src/index.js
@@ -1,31 +1,67 @@
+let users = []
+const usersURL = 'http://localhost:3000/users'
+fetchingUsers()
+
+const divDropDown = document.querySelector('.dropdown')
+const divUserInfo = document.querySelector('.userInfo')
+const selectTag = document.createElement('select')
+
+let opTitle = document.createElement('option')
+opTitle.innerText = "Select a User"
+opTitle.selected = true
+opTitle.disabled = true
+selectTag.append(opTitle)
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM Content Loaded!')
-  
-    let users = []
-    const usersURL = 'http://localhost:3000/users'
-    const divDropDown = document.querySelector('.dropdown')
-    const divUserInfo = document.querySelector('.userInfo')
-    const selectTag = document.createElement('select')
+  console.log('DOM Content Loaded!')
 
-    let opTitle = document.createElement('option')
-    opTitle.innerText = "Select a User"
-    opTitle.selected = true
-    opTitle.disabled = true
-    selectTag.append(opTitle)
-
-    function fetchingUsers(){
-        //request dara from server and call renderUser function for each user
-    }
-
-    function renderUser(user){
-        // creating and appending options to the selectTag for each User
-    }
-
-    selectTag.addEventListener('change', (e) => {
-        //find a user and Use userInfo class to show user Info inside 'user-info' div 
-    })
-
-    divDropDown.append(selectTag)
-    fetchingUsers()
-  
+  selectTag.addEventListener('change', (e) => {
+    //find a user and Use userInfo class to show user Info inside 'user-info' div
+    let id = e.target.selectedIndex
+    let selectedUser = users.find((user) => {return user.id == id})
+    selectedUser.renderUserInfo()
   })
+  divDropDown.append(selectTag)
+
+
+})
+
+//functions
+
+  function fetchingUsers(){
+      //request dara from server and call renderUser function for each user
+      fetch(usersURL)
+        .then((res)=>{return res.json()})
+        .then((data)=>{
+          data.forEach((user)=>{
+            let userObj = new UserInfo(user.id,user.name,user.image,user.calories.total_calories)
+            users.push(userObj)
+            renderUser(user)
+          })
+        })
+  }
+
+  function renderUser(user){
+    // creating and appending options to the selectTag for each User
+    let opTitle = document.createElement('option')
+    opTitle.innerText = user.name
+    selectTag.append(opTitle)
+  }
+
+  function editCalories(user,newCal){
+    fetch(`${usersURL}/${user.id}`,{
+      method: 'PATCH',
+      headers: {
+        'content-type':'application/json'
+      },
+      body: JSON.stringify({
+        "calories": {
+          "total_calories": newCal
+      }
+      })
+      })
+      .then((resp)=>{return resp.json()})
+      .then((data)=>{
+        divUserInfo.children[0].children[2].innerText = data.calories.total_calories
+      })
+  }
--- src/userInfo.js
+++ src/userInfo.js
@@ -1,8 +1,65 @@
 class UserInfo{
-   
-    renderUserInfo(user){
-        
-    }
+  constructor(id,name,image,total_calories=0){
+    this._id = id
+    this.name = name
+    this.image = image
+    this.calories = new Calories()
+    this.calories.total_calories = total_calories
+  }
 
+  get id(){
+    return this._id
+  }
+
+  renderUserInfo(){
+    //clear current content
+    divUserInfo.innerHTML = ""
+    //create div
+    let div = document.createElement('div')
+    //create name tag
+    let nameTag = document.createElement('p')
+    nameTag.innerText = this.name
+    //create image tag
+    let imgTag = document.createElement('img')
+    imgTag.src = this.image
+    //create calories tag
+    let caloriesTag = document.createElement('h4')
+    caloriesTag.innerText = this.calories.total_calories
+    //create calories form
+    let caloriesForm = document.createElement('form')
+    let enterCals = document.createElement('input')
+    enterCals.type = "text"
+    enterCals.placeholder = "Enter Calories"
+    let addCals = document.createElement('input')
+    addCals.type = "submit"
+    addCals.value = "Add Calories"
+    caloriesForm.append(enterCals,addCals)
+    caloriesForm.addEventListener('submit',(e) => {
+      e.preventDefault()
+      let name = e.target.parentElement.children[0].innerText
+      let currentCal = e.target.parentElement.children[2].innerText
+      let addCal = e.target.children[0].value
+      let user = users.find((user)=>{return user.name == name})
+      let newCal = parseInt(currentCal) + parseInt(addCal)
+      // debugger
+      editCalories(user,newCal)
+      user.calories.total_calories = newCal
+      user.renderUserInfo()
+    })
+    // create reset button
+    let reset = document.createElement('button')
+    reset.innerText = "Reset Calories"
+    reset.addEventListener('click',(e)=>{
+      e.preventDefault
+      let name = e.target.parentElement.children[0].innerText
+      let user = users.find((user)=>{return user.name == name})
+      editCalories(user,0)
+      user.calories.total_calories = 0
+      user.renderUserInfo()
+    })
+    //append div
+    div.append(nameTag,imgTag,caloriesTag,caloriesForm,reset)
+    divUserInfo.append(div)
+  }
 
 }
